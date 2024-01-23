7d6d502e9f316bcacdb08f7c41d0a3d5494903a8
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-23 16:25:50 +0000

    add: tasks 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -8,6 +8,4 @@ def matrix_shape(matrix):
     while type(matrix) is list:
         matrix_shape.append(len(matrix))
         matrix = matrix[0]
-
-
-     return matrix_shape
+    return matrix_shape
