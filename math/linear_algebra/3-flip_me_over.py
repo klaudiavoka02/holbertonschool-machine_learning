64c89a1540fb735c4460dd148eeaa6ed2ed557bb
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-25 14:41:00 +0000

    task 3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" returns the transpose of a 2D matrix """
+
+
+def matrix_transpose(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+#create a empty matrix 
+    transpose = [[0] * rows for _ in range(cols)]
+#iterate over rows
+#iterate over cols
+#transpose [j][i] = matrix[i][j]
+for i in range(rows):
+    for j in range(cols):
+        transpose[j][i] = matrix[i][j]
+
+    return transpose
