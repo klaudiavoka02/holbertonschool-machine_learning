097631ad51a8e6128a6d59464bf3413fb122445b
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-25 14:42:41 +0000

    task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,11 +6,8 @@ def matrix_transpose(matrix):
     rows = len(matrix)
     cols = len(matrix[0])
 
-#create a empty matrix 
     transpose = [[0] * rows for _ in range(cols)]
-#iterate over rows
-#iterate over cols
-#transpose [j][i] = matrix[i][j]
+
 for i in range(rows):
     for j in range(cols):
         transpose[j][i] = matrix[i][j]
