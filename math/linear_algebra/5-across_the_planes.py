7cf6b4228b8d82223814b1617abe4edbd2f41561
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-25 15:07:30 +0000

    task 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -10,6 +10,6 @@ def add_matrices2D(mat1, mat2):
     for i in range(len(mat1)):
         row = []
         for j in range(len(mat1[0])):
-            row.append(mat1[i][j] + mat2[i][j]):
+            row.append(mat1[i][j] + mat2[i][j])
         result.append(row)
     return result
