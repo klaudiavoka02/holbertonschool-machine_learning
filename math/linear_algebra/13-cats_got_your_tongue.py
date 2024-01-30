69eeb51bec4d6035236486b2d6b0f0f6eebc58a8
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-30 15:03:21 +0000

    add:task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -2,6 +2,7 @@
 """a script that concatenates two matrices along a specific axis"""
 
 
+import numpy as np
 def np_cat(mat1, mat2, axis=0)
     """function that concatenates two matrices"""
     return np.concatenate((mat1, mat2), axis=axis)
