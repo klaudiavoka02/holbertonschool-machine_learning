c38c09c7ea1bb1a1b5b4632b87a9cbd7df3434af
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-30 15:07:36 +0000

    add:task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a script that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """function that multiplies a matrix"""
+    return np.matmul(mat1, mat2)
