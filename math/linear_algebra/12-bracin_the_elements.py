824bcced3c9037d1716ba3dc30e701a8dfdd439d
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-30 14:44:30 +0000

    add:task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""a script that performs element-wise addition, subtraction, multiplication, and division"""
+
+
+def np_elementwise(mat1, mat2):
+    """a function that performs various operations"""
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
