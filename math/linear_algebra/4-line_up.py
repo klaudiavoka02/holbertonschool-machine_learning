8b32348a3eaf1565aa631282244a069d299c0d05
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-01-31 18:34:05 +0000

    add: task 4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-"""Calculates the shape of a matrix"""
+"""
+A function that adds two arrays element-wise
+"""
 
 
 def add_arrays(arr1, arr2):
-
-
+    """
+A function that adds two arrays element-wise
+    """
     if len(arr1) != len(arr2):
         return None
+
     result = []
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
+
     return result
