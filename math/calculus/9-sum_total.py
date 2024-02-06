33879863e2427d2fcbfed432e5e3a67461890069
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-02-06 13:32:47 +0000

    add :task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
- A scripts that calculates the sum of squares 
- """
+A script that calculates the sum of squares
+"""
 
 
 def summation_i_squared(n):
-"""
-function that calculate the sum of squares
-"""
-    if type(n) is not int:
-       return None
-    sum = (n*(n+1)*(2*n+1)) / 6
+    '''
+    A function that calculates the sum of squares
+    '''
+    if type(n) is not int or n < 1:
+        return None
+    sum = (n*(n+1)*(2*n+1))/6
     return int(sum)
