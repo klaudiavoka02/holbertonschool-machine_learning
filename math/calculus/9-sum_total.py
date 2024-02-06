a5ffad1c8ae8ef7679e66e72f751e515ab499e5c
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-02-06 12:44:32 +0000

    add :task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A scripts tha calculates the sum of squares """
+
+
+def summation_i_squared(n):
+    sum = 0
+    for i in range(n +1):
+        sum += 1 ** 2
+
+    return sum
