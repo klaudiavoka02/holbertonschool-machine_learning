5bcb67afe2eb3daa1c9ddbe36df1d1a43ba8ea6a
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-02-06 13:10:04 +0000

    add :task 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
-""" ... """
+""" 
+A script that calculates the derivative of a polynomial
+ """
 
 
 def poly_derivative(poly):
-"""..."""
+"""a function  that calculates the derivative of a polynomial"""
+    if type(poly) is not list or len(poly) < 1:
+       return None
+    for coeff in poly:
+        if typt(coeff) is not int:
+            return None
