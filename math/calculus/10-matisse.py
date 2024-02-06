d30fb3ed0d927b750709db83eb13bf7db5d1cb86
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-02-06 13:00:39 +0000

    add :task 9

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" ... """
+
+
+def poly_derivative(poly):
+"""..."""
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-""" A scripts that calculates the sum of squares """
+"""
+ A scripts that calculates the sum of squares 
+ """
 
 
 def summation_i_squared(n):
-"""function that calculate the sum of squares"""
+"""
+function that calculate the sum of squares
+"""
     if type(n) is not int:
        return None
     sum = (n*(n+1)*(2*n+1)) / 6
