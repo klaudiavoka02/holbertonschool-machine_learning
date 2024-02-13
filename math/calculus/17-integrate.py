0a9f44caef8ce8c58a41e9f097141d043bf746fe
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-02-13 19:07:15 +0000

    add : task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-""" ... """
+"""
+calculates the integral of a polynomial without importing
+"""
 
 
 def poly_integral(poly, C=0):
-    """ ... """
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly) or not isinstance(C, (int, float)):
+    """
+    :param poly: the given polynomial
+    :param C: a constant value
+    :return: The integral of the polynomial
+    """
+    if not isinstance(poly, list):
         return None
-    
-    integral = [C]
-    for i in range(len(poly)):
-        if not isinstance(poly[i], (int, float)):
-            return None
-        if i == 0:
-            integral.append(poly[i] / (i + 1))
-        else:
-            integral.append(poly[i] / (i + 1))
-    return integral
-
-poly = [5, 3, 0, 1]
-integral = poly_integral(poly, C=0)
-print(integral)  
+    if not isinstance(C, int):
+        return None
+    if not poly:
+        return None
+    integral_result = [coef / (index + 1) for index, coef in enumerate(poly)]
+    integral_result.insert(0, C)
+    integral_result = [int(coef) if int(coef) == coef
+                       else coef for coef in integral_result]
+    while integral_result[-1] == 0 and len(integral_result) > 1:
+        integral_result.pop()
+    return integral_result
