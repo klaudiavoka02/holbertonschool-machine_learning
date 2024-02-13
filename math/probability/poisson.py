297b1320af678300100e2cf8e29ebd77cc6a39fc
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-02-13 19:14:43 +0000

    add : task 0,1,2

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-'''
-A class that represemts a poisson distribution.
-'''
+"""
+represent a poisson distribution
+"""
 
 
 class Poisson:
-    '''
-A class that represemts a poisson distribution.
-    '''
-    def _init_(self, data=None, lambtha=1.):
+    """
+    the poisson distribution class
+    """
+    def __init__(self, data=None, lambtha=1):
+        """
+        represents a poisson distribution
+        """
         self.lambtha = float(lambtha)
         if self.lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
@@ -17,36 +20,33 @@ A class that represemts a poisson distribution.
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data)) / len(data)
+            self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
-        '''
-        A method that calculates the value of the PMF
-        for a given number of “successes”
-        '''
-        k = int(k)
-
+        """
+        ...
+        """
+        if type(k) is not int:
+            k = int(k)
         if k < 0:
             return 0
-
+        e = 2.7182818285
+        lambtha = self.lambtha
         factorial = 1
         for i in range(1, k + 1):
             factorial *= i
-
-        e = 2.7182818285
-        pmf = ((e * -self.lambtha) * (self.lambtha * k)) / factorial
+        pmf = ((e ** -lambtha) * lambtha ** k) / factorial
         return pmf
 
     def cdf(self, k):
-        '''
-        A method that calculates the value of the CDF
-        for a given number of “successes”
-        '''
-
-        k = int(k)
+        """
+        Calculates the cumulative distribution for a given number 
+        """
+        if type(k) is not int:
+            k = int(k)
         if k < 0:
             return 0
         cdf = 0
-        for i in range(k+1):
+        for i in range(k + 1):
             cdf += self.pmf(i)
         return cdf
