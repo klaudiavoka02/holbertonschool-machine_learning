a21f442d7c7ad39b8c053d941d5c607491b9d3ae
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-02-20 15:26:33 +0000

    add : task

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-""" 
-that calculates the likelihood of obtaining 
-this data given various hypothetical probabilities of developing severe side effects
 """
-
+ The function calculates the likelihood 
+"""
 import numpy as np
 
+
 def likelihood(x, n, P):
-    """ calculates the likelihood """
-    if type(n) is not int or n <= 0:
-        raise ValueError(n must be a positive integer)
-    if type(x) is not int or x < 0:
-        raise ValueError(x must be an integer that is greater than or equal to 0)
-    if x >n 
-        raise ValueError(x cannot be greater than n)
-    if type(P) is not np.ndarray or len(P.shape) != 1:
-        raise ValueError(P must be a 1D numpy.ndarray)
-    for value in P:
-        if value > 1 or value < 0:
-            raise ValueError(All values in P must be in the range [0, 1])
-
-    factorial = np.math.factorial
-    fact_coefficent = factorial(n) / (factorial(x) * factorial(n-x))
-    likelihood = fact_coefficent * (P ** x )* ((1-P) ** (n-x))
-
-    return likelihood
+    """
+    Calculate the hypothetical likelihood of severe side effects
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is greater than "
+                         "or equal to 0")
+    if type(P) is not np.ndarray or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    n_factorials = np.math.factorial(n)
+    x_factorial = np.math.factorial(x)
+    likelihoods = ((n_factorials / (x_factorial * np.math.factorial(n - x)))
+                   * (P ** x) * ((1 - P) ** (n - x)))
+    return likelihoods
