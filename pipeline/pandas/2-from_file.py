9f6fcc75d1972d0ab33b3df87ba17b43fffb517a
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-03-04 14:47:00 +0000

    add : task 2

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import datetime
+
+def from_file(filename, delimiter):
+
+    df = pd.read.csv(filename, delimiter=delimiter)
+    return df
+
+df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+print(df1.head())
