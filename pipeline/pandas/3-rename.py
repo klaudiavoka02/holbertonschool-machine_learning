e39f2dc87d211b0adf40397233d5121a8a16e85a
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-03-04 14:29:24 +0000

    add : task 3

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import datetime
+# Old file required from previous task
+
+def from_file(filename, delimiter):
+
+    data = pd.read_csv(filename, delimiter=delimiter, engine='python')
+    return data
+file = pd.read_csv('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv')
+file.rename(columns={"Timestamp": "Datetime"}, inplace=True)
+file['Datetime'] = pd.to_datetime(file['Datetime'], unit='s')
+print(file[['Datetime', 'Close']].tail())
