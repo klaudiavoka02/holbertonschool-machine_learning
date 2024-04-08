a7b7c013ac73d72d88f07068a2c287611d4e7470
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-04-08 14:15:46 +0000

    task 2

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Optimize
+"""
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+        function that sets up Adam optimization for a keras model
+        with categorical crossentropy loss and accuracy metrics
+    """
+    Adam_optimizer = K.optimizers.Adam(learning_rate=alpha,
+                                       beta_1=beta1,
+                                       beta_2=beta2)
+
+    network.compile(optimizer=Adam_optimizer,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
