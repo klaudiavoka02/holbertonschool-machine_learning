ad6fddb8684ef7045522570db008fa128b28487d
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-04-08 13:19:27 +0000

    task 1

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    Input
+"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+        function that builds a neural network with the Keras library
+    """
+    inputs = K.Input(shape=(nx,))
+
+    x = inputs
+    for i in range(len(layers)):
+
+        x = K.layers.Dense(layers[i],
+                           activation=activations[i],
+                           kernel_regularizer=K.regularizers.L2(lambtha))(x)
+
+
+        if i != len(layers) - 1 and keep_prob is not None:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+
+
+    model = K.Model(inputs, x)
+
+    return model
