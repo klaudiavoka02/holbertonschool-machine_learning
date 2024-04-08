e84c7157153ee4cbdbda0e9bc2c4f1c5f42c1645
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-04-08 14:07:55 +0000

    task 3

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,12 @@
+"""
+    One Hot
+"""
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+        function that converts a label vector into a one-hot matrix
+    """
+    return K.utils.to_categorical(labels, classes)
