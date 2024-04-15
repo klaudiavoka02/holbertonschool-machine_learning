f58fe20ad66d9e389dd06775b49d26ffe346b9c0
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-04-15 13:26:54 +0000

    task 5

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Train
+"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, verbose=True, shuffle=False):
+    """
+        Function that trains a model using mini-batch gradient descent
+    """
+    history = network.fit(x=data,
+                          y=labels,
+                          epochs=epochs,
+                          batch_size=batch_size,
+                          validation_data=validation_data,
+                          verbose=verbose,
+                          shuffle=shuffle)
+
+    return history
