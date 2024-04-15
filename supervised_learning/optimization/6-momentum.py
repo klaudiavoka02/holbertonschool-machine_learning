5e07157cb8734c078a800f38cb55b6408b273be5
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-04-15 14:22:00 +0000

    task 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+   Momentum upgraded
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+        Method to creates the training operation for a NN
+        in tf using gradient descent with momentum opt algo
+    """
+
+    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha,
+                                           momentum=beta1)
+
+    train_op = optimizer.minimize(loss)
+
+    return train_op
