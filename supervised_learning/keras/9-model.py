842eea842a31837e054c8073214a01481e863ee5
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-04-15 13:30:46 +0000

    task 9

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Save and load model function
+"""
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+        function that saves an entire model
+    """
+    network.save(filename)
+
+
+def load_model(filename):
+    """
+        function that loads an entire model
+
+        :param filename: path where model loaded from
+
+        :return: loaded model
+    """
+    return K.models.load_model(filename)
