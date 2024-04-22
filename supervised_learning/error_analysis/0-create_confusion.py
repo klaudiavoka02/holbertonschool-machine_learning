70b7bba2f2ef4b6b3a5fcce9191e8857ef2eac77
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-04-22 13:53:29 +0000

    task o

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    Matrix confusion
+"""
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+        function that creates a confusion matrix
+    """
+
+    m = labels.shape[0]
+    classes = labels.shape[1]
+    conf_matrix = np.zeros((classes, classes))
+
+    
+    for i in range(m):
+        true_class = np.argmax(labels[i])
+        predicted_class = np.argmax(logits[i])
+
+        conf_matrix[true_class, predicted_class] += 1
+
+    return conf_matrix
