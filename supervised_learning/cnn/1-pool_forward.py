fad92a959874e0903a98ed50810b291469e16c2f
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-05-09 15:02:53 +0000

    task 1

---
+++ supervised_learning/cnn/1-pool_forward.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    Pooling Forward Propagation
+"""
+
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """
+        function that performs forward propagation over a pooling layer
+        of a NN
+    """
+
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    output_height = int((h_prev - kh) / sh + 1)
+    output_width = int((w_prev - kw) / sw + 1)
+
+    pooled_img = np.zeros((m, output_height, output_width, c_prev))
+
+    for i in range(output_height):
+        for j in range(output_width):
+            image_zone = A_prev[:, i * sh:i * sh + kh,
+                                j * sw:j * sw + kw, :]
+
+            if mode == 'max':
+                pooled_img[:, i, j, :] = np.max(image_zone, axis=(1, 2))
+            elif mode == 'avg':
+                pooled_img[:, i, j, :] = np.average(image_zone, axis=(1, 2))
+
+    return pooled_img
