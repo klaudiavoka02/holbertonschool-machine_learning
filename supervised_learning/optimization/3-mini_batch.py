053ae44349261c9c0103b23eca3a4b7c29fcaafd
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-05-13 13:31:52 +0000

    task 3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,71 +1,51 @@
 #!/usr/bin/env python3
 """
-   Mini-batch
+Defines function that trains a loaded neural network model
+using mini-batch gradient descent
 """
 
-import tensorflow.compat.v1 as tf
 
+import tensorflow as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
-                     epochs=5, load_path="/tmp/model.ckpt",
-                     save_path="/tmp/model.ckpt"):
+def create_mini_batches(X, Y, batch_size):
     """
-        Function trains a loaded neural network model using
-        mini-batch gradient descent
+    Creates mini-batches to be used for training
+    a neural network using mini-batch gradient descent.
+
+    Args:
+        X (numpy.ndarray): Input data of shape (m, nx).
+        Y (numpy.ndarray): Labels of shape (m, ny).
+        batch_size (int): Number of data points in a batch.
+
+    Returns:
+        list: List of mini-batches containing tuples (X_batch, Y_batch).
     """
+    m = X.shape[0]
+    mini_batches = []
 
-    with tf.Session() as sess:
-        new_saver = tf.train.import_meta_graph(load_path + ".meta")
-        new_saver.restore(sess, load_path)
+    # Shuffle the data
+    X_shuffled, Y_shuffled = shuffle_data(X, Y)
 
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-        train_op = tf.get_collection('train_op')[0]
+    # Calculate the number of batches
+    num_batches = m // batch_size
 
-        m = X_train.shape[0]
+    for i in range(num_batches):
+        start_idx = i * batch_size
+        end_idx = start_idx + batch_size
 
-        for epoch in range(epochs + 1):
+        X_batch = X_shuffled[start_idx:end_idx]
+        Y_batch = Y_shuffled[start_idx:end_idx]
 
-            train_cost, train_accuracy = sess.run(
-                [loss, accuracy], feed_dict={x: X_train, y: Y_train})
+        mini_batches.append((X_batch, Y_batch))
 
-            valid_cost, valid_accuracy = sess.run(
-                [loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+    # Handle the last batch
+    # (if the total number of data points is not divisible by batch_size)
+    if m % batch_size != 0:
+        start_idx = num_batches * batch_size
+        X_batch = X_shuffled[start_idx:]
+        Y_batch = Y_shuffled[start_idx:]
+        mini_batches.append((X_batch, Y_batch))
 
-            print("After {} epochs:".format(epoch))
-            print("\tTraining Cost: {}".format(train_cost))
-            print("\tTraining Accuracy: {}".format(train_accuracy))
-            print("\tValidation Cost: {}".format(valid_cost))
-            print("\tValidation Accuracy: {}".format(valid_accuracy))
-
-            if epoch < epochs:
-                X_train_shuffled, Y_train_shuffled = \
-                    shuffle_data(X_train, Y_train)
-
-                nbr_batch = m // batch_size + (m % batch_size != 0)
-
-                for step_number in range(nbr_batch):
-                    first_index = step_number * batch_size
-                    last_index = min(first_index + batch_size, m)
-
-                    x_batch = X_train_shuffled[first_index: last_index]
-                    y_batch = Y_train_shuffled[first_index: last_index]
-
-                    sess.run(train_op, feed_dict={x: x_batch, y: y_batch})
-
-                    if step_number > 0 and (step_number + 1) % 100 == 0:
-                        step_cost, step_accuracy = sess.run(
-                            [loss, accuracy],
-                            feed_dict={x: x_batch, y: y_batch})
-
-                        print("\tStep {}:".format(step_number + 1))
-                        print("\t\tCost: {}".format(step_cost))
-                        print("\t\tAccuracy: {}".format(step_accuracy))
-
-        saved_model = new_saver.save(sess, save_path)
-
-        return saved_model
+    return mini_batches
