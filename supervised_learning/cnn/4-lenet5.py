e2d82766ced1077bc3eba4547339ac61662b38a3
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-05-13 13:59:02 +0000

    task 4

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -11,13 +11,15 @@ def lenet5(x, y):
         function that builds a modified version of the LeNet-5
         architecture using TensorFlow version 1
     """
+
     initializer = tf.keras.initializers.VarianceScaling(scale=2.0)
+
     conv1 = tf.layers.Conv2D(filters=6,
                              kernel_size=5,
                              padding='same',
                              kernel_initializer=initializer,
                              activation='relu')(x)
-                              pool1 = tf.layers.MaxPooling2D(pool_size=2,
+    pool1 = tf.layers.MaxPooling2D(pool_size=2,
                                    strides=2)(conv1)
     conv2 = tf.layers.Conv2D(filters=16,
                              kernel_size=5,
@@ -26,6 +28,7 @@ def lenet5(x, y):
                              activation='relu')(pool1)
     pool2 = tf.layers.MaxPooling2D(pool_size=2,
                                    strides=2)(conv2)
+
     flat = tf.layers.Flatten()(pool2)
     full1 = tf.layers.Dense(120,
                             activation='relu',
@@ -41,12 +44,15 @@ def lenet5(x, y):
     loss = tf.losses.softmax_cross_entropy(
         onehot_labels=y,
         logits=output)
+
     train_Adam = tf.train.AdamOptimizer().minimize(loss)
 
     y_pred = tf.argmax(output, axis=1)
     y_true = tf.argmax(y, axis=1)
     correct_prediction = tf.equal(y_pred, y_true)
+
     correct_prediction = tf.cast(correct_prediction, dtype=tf.float32)
+
     accuracy = tf.reduce_mean(correct_prediction)
 
     return softmax, train_Adam, loss, accuracy
