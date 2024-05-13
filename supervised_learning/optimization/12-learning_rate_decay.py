19c0a9faab305db0bcf4447b2614ee31f2c92468
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-05-13 13:28:23 +0000

    task 12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -3,21 +3,18 @@
    Learning Rate decay upgraded
 """
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
-def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+def learning_rate_decay(alpha, decay_rate, decay_step):
     """
-        Method that creates learning rate decay operation in tf
-        using inverse time decay
+    Creates a learning rate decay operation
+    in TensorFlow using inverse time decay.
     """
-    # set train exponential decay in tf
-    # use staircase=True to occur in a stepwise fashion
-    learning_rate = tf.compat.v1.train.inverse_time_decay(
-        learning_rate=alpha,
-        decay_steps=decay_step,
+    learning_rate_decay_op = tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
         decay_rate=decay_rate,
-        global_step=global_step,
-        staircase=True)
-
-    return learning_rate
+        decay_steps=decay_step,
+        staircase=True
+    )
+    return learning_rate_decay_op
