89c93cc174342942266269581c51bc5d994178c3
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-05-20 15:15:11 +0000

    task 4

---
+++ pipeline/data_augmentation/4-brightness.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Defines function that randomly changes the brightness of an image
+"""
+
+
+import tensorflow as tf
+
+
+def change_brightness(image, max_delta):
+    """
+    Randomly changes the brightness of an image
+    """
+    return (tf.image.random_brightness(image, max_delta))
