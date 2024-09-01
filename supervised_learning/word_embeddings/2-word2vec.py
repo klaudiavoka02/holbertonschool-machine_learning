eb9df9a8a1888837def756b08d6708369bee752c
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-09-01 17:06:31 +0000

    Train Word2Vec

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
-"""Cumulative N-gram BLEU Score"""
-import numpy as np
+""""
+Creates and trains a gensim word2vec model
+"""
 
 
-def cumulative_bleu(references, sentence, n):
+from gensim.models import Word2Vec
+
+
+def word2vec_model(sentences, size=100, min_count=5, window=5,
+                   negative=5, cbow=True, iterations=5, seed=0, workers=1):
     """
-    Calculates the cumulative n-gram BLEU score for a sentence:
-        - references is a list of reference translations
-        - each reference translation is a list of the words in the translation
-        - sentence is a list containing the model proposed sentence
-        - n is the size of the n-gram to use for evaluation
-        - All n-gram scores should be weighted evenly
+    Creates and trains a gensim word2vec model
 
-    Returns: the cumulative n-gram BLEU score
+    Parameters:
+        sentences (list): list of sentences to be trained on
+
+        size (int): dimensionality of embedding layer
+
+        min_count (int): minimum count of words to be considered in training
+
+        window (int): maximum difference between the current predicted word
+        and the sentence
+
+        negative (int): negative sampling
+
+        cbow (boolean): if true, use cbow embeddings, if false, use skip-gram
+
+        iterations (int): number of training iterations
+
+        seed (int): random seed for number generator
+
+        workers (int): number of parallel worker threads to train the model
     """
+    if cbow is True:
+        cbow_flag = 0
+    if cbow is False:
+        cbow_flag = 1
 
-    BP = min(1, np.exp(1 - len(min(references, key=len)) / len(sentence)))
-    precision = []
-
-    for m in range(1, n+1):
-        n_grams = []
-        for reference in references:
-            n_grams_ref = []
-            for i in range(len(sentence) - (m - 1)):
-                if any(sentence[i:i + m] == reference[j:j+m]
-                       for j in range(len(reference) - (m - 1))) and \
-                        sentence[i:i+m] not in n_grams_ref:
-                    n_grams_ref.append(sentence[i:i+m])
-            n_grams.append(len(n_grams_ref))
-        precision.append(max(n_grams) / (i + 1))
-
-    return BP * np.exp(np.mean(np.log(precision)))
+    return Word2Vec(sentences=sentences, size=size, min_count=min_count,
+                    window=window, negative=negative, iter=iterations,
+                    seed=seed, workers=workers, sg=cbow_flag)
