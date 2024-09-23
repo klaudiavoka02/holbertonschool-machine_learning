646808bbbc316b0af96aef117c2ab39a5819b67c
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-09-23 14:27:13 +0000

    Agglomerative

--- unsupervised_learning/clustering/12-agglomerative.py
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -1,40 +1,15 @@
 #!/usr/bin/env python3
-"""
-Performs agglomerative clustering on a dataset
-"""
-
-
+"""Agglomerative Module"""
 import scipy.cluster.hierarchy
 import matplotlib.pyplot as plt
 
 
 def agglomerative(X, dist):
-    """
-    Performs agglomerative clustering on a dataset
+    """ Performs agglomerative clustering on a dataset """
+    dendrogram = scipy.cluster.hierarchy.linkage(X, method='ward')
+    scipy.cluster.hierarchy.dendrogram(dendrogram, color_threshold=dist)
 
-    Parameters:
-        X: numpy array of shape (n, d) containing the dataset
-            -> n: number of datapoints
-            -> d: dimensionality of the dataset
-
-        dist: maximum cophenetic distance
-
-    Performs agglomerative clustering with a Ward Linkage
-
-    Displays the dendrogram with each cluster displayed in a different colour
-
-    Returns:
-        clss: clss, a numpy.ndarray of shape (n,) containing the cluster
-        indices for each data point
-    """
-    ward = scipy.cluster.hierarchy.linkage(X, method='ward')
-
-    plt.figure()
-    scipy.cluster.hierarchy.dendrogram(ward, color_threshold=dist)
-    plt.title('Agglomerative clustering')
-    plt.xlabel('Data points')
-    plt.ylabel('Distance')
     plt.show()
 
-    clss = scipy.cluster.hierarchy.fcluster(ward, dist, criterion='distance')
-    return clss
+    return scipy.cluster.hierarchy.fcluster(dendrogram, dist,
+                                            criterion='distance')
