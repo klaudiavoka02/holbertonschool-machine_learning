66486bb5c80bfa647e6d660506d8644eeb5ec2b3
klaudiavoka02 <kvoka20@epoka.edu.al>
2024-09-23 10:25:03 +0000

    Extract Word2Vec

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -7,5 +7,10 @@ Converts a gensim word2vec model to a keras model
 def gensim_to_keras(model):
     """
     Converts a gensim word2vec model to a keras model
-"""
+
+    Parameters:
+        model: gensim word2vec model which is trained
+    Returns:
+        Keras Embedding layer
+    """
     return model.wv.get_keras_embedding(True)
